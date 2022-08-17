279cd98ca85958e13a32fd7f759f4093b3efa9dc
gabriellukke <gabriellukke@outlook.com>
2022-08-17 19:49:00 -0300

    feat: sequelize initial config

---
+++ src/database/config/config.js
@@ -0,0 +1,29 @@
+require('dotenv/config');
+
+const config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+  },
+};
+
+const environment = process.env.APP_ENV || 'development';
+
+module.exports = config[environment];
