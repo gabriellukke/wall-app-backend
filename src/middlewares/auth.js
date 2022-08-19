dd55a54f8a7d314fd04f16e9b88ef66dae2edcf8
gabriellukke <gabriellukke@outlook.com>
2022-08-19 03:39:24 -0300

    feat: create token auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,18 @@
+require('dotenv/config');
+const jwt = require('jsonwebtoken');
+
+const secretKey = process.env.SECRET_KEY || '2c85c7d5da3941bffefd6920fa029112';
+
+const createToken = (payload) => {
+  try {
+    const options = {
+      algorithm: 'HS256',
+      expiresIn: '12h',
+    };
+  
+    const token = jwt.sign(payload, secretKey, options);
+    return token;
+  } catch (error) {
+    throw error;
+  }
+};
