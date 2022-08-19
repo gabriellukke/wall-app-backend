aca3b1995af8bd1879b542e298b3930cb51f0f27
gabriellukke <gabriellukke@outlook.com>
2022-08-19 09:56:31 -0300

    refact: change userModel method to findOne on verification of an existing user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,14 +3,12 @@ const { userModel } = require('../database/models');
 const createUser = async (userInfo) => {
   const { email } = userInfo;
 
-  const dbUser = await userModel.findAll({ where: { email } });
-
-  if (dbUser.length) return { message: 'E-mail already registered!' };
+  const dbUser = await userModel.findOne({ where: { email } });
+  if (dbUser) return { message: 'E-mail already registered!' };
 
   const {
     dataValues: { id },
   } = await userModel.create(userInfo);
-
   return id;
 };
 
