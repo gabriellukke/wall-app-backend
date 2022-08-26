f3a8fbc0a94e9655d8f7682aaeb59a0f66521e76
gabriellukke <gabriellukke@outlook.com>
2022-08-26 01:59:09 -0300

    fix: service key in transporter when using nodemailer testAccount

--- src/middlewares/sendEmail.js
+++ src/middlewares/sendEmail.js
@@ -3,33 +3,32 @@ const nodemailer = require('nodemailer');
 
 module.exports = async (email, firstName) => {
   const testAccount = await nodemailer.createTestAccount();
-  try {
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.NODEMAILER_EMAIL,
+      pass: process.env.NODEMAILER_PASSWORD,
+    },
+    ...(process.env.NODE_ENV === 'development' || 'test' && {
+      service: null,
+      host: 'smtp.ethereal.email',
+      port: 587,
       auth: {
-        user: process.env.NODEMAILER_EMAIL,
-        pass: process.env.NODEMAILER_PASSWORD,
+        user: testAccount.user,
+        pass: testAccount.pass,
       },
-      ...(process.env.NODE_ENV === 'development' && {
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-          user: testAccount.user,
-          pass: testAccount.pass,
-        },
-      }),
-    });
+    }),
+  });
 
-    await transporter.sendMail({
-      from: `'The Wall App Team' ${
-        process.env.NODEMAILER_EMAIL || 'app@wall.com'
-      }`,
-      to: email,
-      subject: 'Welcome to The Wall App',
-      text: `Hi ${firstName} 👋
+  console.log(transporter);
+
+  await transporter.sendMail({
+    from: `'The Wall App Team' ${
+      process.env.NODEMAILER_EMAIL || 'app@wall.com'
+    }`,
+    to: email,
+    subject: 'Welcome to The Wall App',
+    text: `Hi ${firstName} 👋
       Welcome to The Wall App! 🎉`,
-    });
-  } catch (error) {
-    throw error;
-  }
+  });
 };
