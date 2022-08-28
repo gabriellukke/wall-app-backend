508edba01130abf45309b67ba41e44ce303fe695
gabriellukke <gabriellukke@outlook.com>
2022-08-28 06:59:21 -0300

    refact: separate the app and server

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
 const { userRouter, postRouter } = require('./routes');
@@ -8,9 +7,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
-
 app.use('/user', userRouter);
 app.use('/post', postRouter);
 
-app.listen(PORT, () => console.log(`API's running on PORT: ${PORT}`));
+module.exports = app;
+
