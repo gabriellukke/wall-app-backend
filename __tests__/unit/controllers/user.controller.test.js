309d4b3837de955c06f8b9b733deb08981fd0424
gabriellukke <gabriellukke@outlook.com>
2022-08-30 01:21:47 -0300

    test: user controller, login cases

--- __tests__/unit/controllers/user.controller.test.js
+++ __tests__/unit/controllers/user.controller.test.js
@@ -10,7 +10,7 @@ describe('User Controller', () => {
 
   after(() => {
     userService.createUser.restore();
-    // userService.login.restore();
+    userService.login.restore();
   });
 
   it('Should return error message when user already exists', async () => {
@@ -31,7 +31,7 @@ describe('User Controller', () => {
     userService.createUser.restore();
   });
 
-  it('should create a user', async () => {
+  it('Should create/register a user', async () => {
     request.body = {
       email: 'gabriel@test.com',
     };
@@ -46,5 +46,39 @@ describe('User Controller', () => {
     await userController.createUser(request, response);
     expect(response.status.calledWith(200)).to.be.true;
     expect(response.json.calledWith({ message: 'User registered successfully' })).to.be.true;
+  });
+
+  it('Should return a error message when user service login method does not match credentials', async () => {
+    request.body = {
+      email: 'gabriel@test.com',
+    };
+
+    response.status = sinon.stub().returns(response);
+    response.json = sinon.stub().returns();
+    sinon.stub(userService, 'login').resolves({
+      message: 'Unauthorized, incorrect or unregistered email',
+    });
+
+    await userController.login(request, response);
+    expect(response.status.calledWith(401)).to.be.true;
+    expect(response.json.calledWith({ message: 'Unauthorized, incorrect or unregistered email' })).to.be.true;
+    userService.login.restore();
+  });
+
+  it('Should return a token and user information when user service login method matches credentials', async () => {
+    request.body = {
+      email: 'gabriel@test.com',
+    };
+
+    response.status = sinon.stub().returns(response);
+    response.json = sinon.stub().returns();
+    sinon.stub(userService, 'login').resolves({
+      token: 'token',
+      ...request.body
+    });
+
+    await userController.login(request, response);
+    expect(response.status.calledWith(200)).to.be.true;
+    expect(response.json.calledWith({ token: 'token', ...request.body })).to.be.true;
   });
 });
